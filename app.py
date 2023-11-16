36971134015396b0024b54319225816452613dda
Vijit Singh <sports.football99@gmail.com>
2023-11-16 10:59:06 +0530

    'embarkedoheremoval'

--- app.py
+++ app.py
@@ -2,24 +2,23 @@ import streamlit as st
 import numpy as np
 from pickle import load
 
-scaler = load(open('models/standard_scaler.pkl', 'rb'))
-encoder_ = load(open('models/encoder_.pkl', 'rb'))
-svc_model = load(open('models/svc_model.pkl', 'rb'))
+scaler = load(open('models/standard_scaler_2.pkl', 'rb'))
+model = load(open('models/classifier_2.pkl', 'rb'))
 
 pclass = st.text_input("Pclass", placeholder="Enter Pclass")
 gender = st.text_input("gender", placeholder="Enter Sex")
 age = st.text_input("age", placeholder="Enter age")
-sib = st.text_input("sib", placeholder="SibSp")
-parch = st.text_input("parch", placeholder="Parch")
+alone = st.text_input("are you alone?", placeholder="alone?")
+family = st.text_input("sib", placeholder="Family Size")
 embarked = st.text_input("embarked", placeholder="Embarked")
 
 btn_click = st.button("Predict")
 
 if btn_click == True:
-    if pclass and gender and age and sib and parch and embarked:
+    if pclass and gender and age and alone and family and embarked:
         age_r = np.array([float(age)]).reshape(-1, 1)
         age_transformed = scaler.transform(age_r)
-        cat = [[int(pclass), str(gender), int(sib), int(parch), str(embarked)]]
+        cat = [[int(pclass), str(gender), int(alone), int(family), str(embarked)]]
         cat_tr = encoder_.transform(cat)
         cat_tr_r = np.array(cat_tr)
         query_point_final = np.concatenate((age_transformed, cat_tr_r), axis=1)
